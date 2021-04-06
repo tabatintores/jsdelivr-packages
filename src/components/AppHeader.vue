79466c8748dda27703b9b5daf9b7d2da1b656cf3
tabatintores <big_clifty@list.ru>
2021-04-06 11:05:57 +0500

    Разработка приложения

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar
+      app
+      color="primary"
+      dark
+  >
+    <v-toolbar-title>jsdelivr-packages</v-toolbar-title>
+    <search class="ml-5"/>
+  </v-app-bar>
+</template>
+
+<script>
+import Search from "@/components/Search";
+
+export default {
+  name: "AppHeader",
+  components: {Search}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-text-field
+      v-model="search"
+      label="Поиск пакета"
+      dark
+      flat
+      solo-inverted
+      hide-details
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+name: "Search"
+}
+</script>
+
+<style scoped>
+
+</style>
